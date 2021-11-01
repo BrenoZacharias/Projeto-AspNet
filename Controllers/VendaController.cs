27ddd6a92566a55c74c8ce71eb6add403c8cd85d
Jean Guilherme <creaty12345@gmail.com>
2021-11-01 20:27:35 -0300

    Continuando Trabalho

---
+++ Controllers/VendaController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Projeto_Loja_Sapatos.Data;
+using Projeto_Loja_Sapatos.Models;
+
+namespace Projeto_Loja_Sapatos.Controllers
+{
+    public class VendaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public VendaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Venda
+        public async Task<IActionResult> Index()
+        {
+            var Cliente = await _context.Clientes.ToListAsync();
+            var Vendas = await _context.Vendas.ToListAsync();
+            var Modelos = await _context.Modelos.ToListAsync();
+            List<VendaViewModel> VendaViewModel = new List<VendaViewModel>(); 
+            foreach
+
+            var appDbContext = _context.Vendas.Include(v => v.Cliente).Include(v => v.Modelo);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Venda/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venda = await _context.Vendas
+                .Include(v => v.Cliente)
+                .Include(v => v.Modelo)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (venda == null)
+            {
+                return NotFound();
+            }
+
+            return View(venda);
+        }
+
+        // GET: Venda/Create
+        public IActionResult Create()
+        {
+            ViewData["id_cliente"] = new SelectList(_context.Clientes, "Id", "CPF");
+            ViewData["id_modelo"] = new SelectList(_context.Modelos, "id", "codigoRef");
+            return View();
+        }
+
+        // POST: Venda/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,id_modelo,id_cliente,quantidade,dtVenda")] Venda venda)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venda);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["id_cliente"] = new SelectList(_context.Clientes, "Id", "CPF", venda.id_cliente);
+            ViewData["id_modelo"] = new SelectList(_context.Modelos, "id", "codigoRef", venda.id_modelo);
+            return View(venda);
+        }
+
+        // GET: Venda/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venda = await _context.Vendas.FindAsync(id);
+            if (venda == null)
+            {
+                return NotFound();
+            }
+            ViewData["id_cliente"] = new SelectList(_context.Clientes, "Id", "CPF", venda.id_cliente);
+            ViewData["id_modelo"] = new SelectList(_context.Modelos, "id", "codigoRef", venda.id_modelo);
+            return View(venda);
+        }
+
+        // POST: Venda/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,id_modelo,id_cliente,quantidade,dtVenda")] Venda venda)
+        {
+            if (id != venda.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venda);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VendaExists(venda.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["id_cliente"] = new SelectList(_context.Clientes, "Id", "CPF", venda.id_cliente);
+            ViewData["id_modelo"] = new SelectList(_context.Modelos, "id", "codigoRef", venda.id_modelo);
+            return View(venda);
+        }
+
+        // GET: Venda/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venda = await _context.Vendas
+                .Include(v => v.Cliente)
+                .Include(v => v.Modelo)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (venda == null)
+            {
+                return NotFound();
+            }
+
+            return View(venda);
+        }
+
+        // POST: Venda/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var venda = await _context.Vendas.FindAsync(id);
+            _context.Vendas.Remove(venda);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VendaExists(int id)
+        {
+            return _context.Vendas.Any(e => e.id == id);
+        }
+    }
+}
